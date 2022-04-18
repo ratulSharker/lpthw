24c5dd3cf512d9e8c92132ef3256e2c9a8d77851
Ratul sharker <sharker.ratul.08@gmail.com>
2022-04-18 17:31:52 +0600

    - gitignore added. - excersice 12 covered.

---
+++ ex12.py
@@ -0,0 +1,8 @@
+name = input("What is your name ? ")
+age = input("How old are you ? ")
+height = input("How tall are you ? ")
+favorite_color = input("What is your favorite color ? ")
+
+print(f"So, your name \"{name}\", age {age}, height \"{height}\", favorite color \"{favorite_color}\"")
+
+# print("What is your name", input())
