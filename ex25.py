e497539199be6c5a57eb85f713ddbe357d15cd87
Ratul sharker <sharker.ratul.08@gmail.com>
2022-04-27 16:52:52 +0600

    - ex25 completed.

--- ex25.py
+++ ex25.py
@@ -19,4 +19,19 @@ def print_last_word(words):
 
 def sort_scentence(sentence):
     words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Print the first and last words of sentence"""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(scetnence):
+    """Print the first and last words of sentence after sorting"""
+    words = break_words(scetnence)
+    sorted_words = sort_words(scetnence)
+    print_first_word(sorted_words)
+    print_last_word(sorted_words)
+
     
